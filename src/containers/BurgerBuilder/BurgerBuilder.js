132d23182b080dc024ed1a21d59a33e167a03dbc
dim3n21 <dima.andrianov@gmail.com>
2018-08-14 10:19:51 -0700

    build the basic components and implement the basic visualisation components

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+class BurgerBuilder extends Component {
+//  constructor(props) {
+//    super(props);
+//    this.state = {};
+
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 2,
+      cheese: 3,
+      meat: 2
+    }
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
