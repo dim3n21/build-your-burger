2a23e4b55e036adc655580bfe09c00497500635c
dim3n21 <dima.andrianov@gmail.com>
2018-08-27 21:25:26 -0700

    update Order component. Update the server side

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,14 +3,34 @@ import classes from './Order.css';
 
 
 const Order = (props) => {
+  console.log(props)
+  const ingredients = [];
 
+  for (let ingredientName in props.ingredients) {
+    ingredients.push(
+      {
+        name: ingredientName,
+        amount: props.ingredients[ingredientName]
+      }
+    );
+  }
+
+  const ingredientOutput = ingredients.map( ig => {
+    return <span
+        style={{textTransform: 'capitalize',
+        display: 'inline-block',
+        border: '1px solid #ccc',
+        margin: '0 8px',
+        padding: '5px'}}
+        key={ig.name}>{ig.name} ({ig.amount})</span>;
+  });
 
   return (
     <div className={classes.Order}>
-      <p>Ingredients: - - - </p>
-      <p>Proce: <strong>USD 5:45</strong></p>
+      <p>Ingredients: {ingredientOutput} </p>
+      <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
     </div>
-  )
-}
+  );
+};
 
 export default Order;
