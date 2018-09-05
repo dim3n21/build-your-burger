22b482f54d2fbfedd5d80211af46e957bdb07b9d
dim3n21 <dima.andrianov@gmail.com>
2018-09-05 15:38:08 -0700

    implement signin and signup authentication through firebase auth REST.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -37,7 +37,8 @@ class Auth extends Component {
                 valid: false,
                 touched: false
             }
-        }
+        },
+        isSignup: true
     }
 
     checkValidity(value, rules) {
@@ -73,7 +74,13 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+    }
+
+    switchAuthModeHandler = () => {
+      this.setState( prevState => {
+        return {isSignup: !prevState.isSignup}
+      });
     }
 
     inputChangedHandler = (event, controlName) => {
@@ -115,6 +122,10 @@ class Auth extends Component {
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
+                    <Button
+                      clicked={this.switchAuthModeHandler}
+                      btnType="Danger">{this.state.isSignup ? 'SWITCH TO SIGN IN' : 'SWITCH TO SIGN UP'}
+                    </Button>
                 </form>
             </div>
         );
@@ -123,7 +134,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
     };
 };
 
